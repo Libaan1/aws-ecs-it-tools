dfd4b892f977997c98b37090ec9696bd11c07a29
Abdi <libaan.abdi@balfourbeatty.com>
2026-08-08 16:31:12 +0100

    Associate public subnets with route table

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -57,3 +57,13 @@ resource "aws_route_table" "public" {
     Name = "it-tools-public-route-table"
   }
 }
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
