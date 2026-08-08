9ae563bfe477e078d1ac1937ab0c1bc0505e01fc
Abdi <libaan.abdi@balfourbeatty.com>
2026-08-08 16:19:46 +0100

    Add public route table

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -44,3 +44,16 @@ resource "aws_internet_gateway" "main" {
     Name = "it-tools-igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "it-tools-public-route-table"
+  }
+}
